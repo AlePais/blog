1fa7dab07eb938a2a574bcc13e97f80bf1242fcf
Alejandro Pais <alejandro.pais.ap@gmail.com>
2022-07-31 22:27:54 -0300

    Agregando las modificaciones

---
+++ resources/views/cursos/create.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.plantilla')
+
+@section('title', "Cre-An-Do, solo estoy Cre-An-Do")
+
+@section('content')
+    <h1>En esta pagina podras crear un curso</h1>
+@endsection
---
+++ resources/views/cursos/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.plantilla')
+
+@section('title', "Curso incierto")
+
+@section('content')
+    <h1>Bienvenido a la pagina cursos</h1>    
+@endsection
