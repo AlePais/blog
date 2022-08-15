7070b6e28c81ada14c247f65c9ec4c09cf444703
Alejandro Pais <alejandro.pais.ap@gmail.com>
2022-08-15 16:09:05 -0300

    Clase 20 - Form request b

--- resources/views/cursos/create.blade.php
+++ resources/views/cursos/create.blade.php
@@ -9,20 +9,41 @@
         <label >
             Nombre:
             <br>
-            <input type="text" name="name">
+            <input type="text" name="name" value="{{old('name')}}">
         </label>
+
+        @error('name')
+            <br>
+            <small style="color:red">*{{$message}}</small>
+            <br>
+        @enderror
+
         <br>
         <label >
             Descripcion:
             <br>
-            <textarea name="description" rows="5"></textarea>
+            <textarea name="description" rows="5">{{old('description')}}</textarea>
         </label>
+        
+        @error('description')
+            <br>
+            <small style="color:red">*{{$message}}</small>
+            <br>
+        @enderror
+
         <br>
         <label >
             Categoria:
             <br>
-            <input type="text" name="categoria">
+            <input type="text" name="categoria" value="{{old('categoria')}}">
         </label>
+        
+        @error('categoria')
+            <br>
+            <small style="color:red">*{{$message}}</small>
+            <br>
+        @enderror
+
         <br>
         <input type="submit" value="Enviar formulario">
     </form>
--- resources/views/cursos/edit.blade.php
+++ resources/views/cursos/edit.blade.php
@@ -12,20 +12,41 @@
         <label >
             Nombre:
             <br>
-            <input type="text" name="name" value="{{$curso->name}}">
+            <input type="text" name="name" value="{{old('name', $curso->name)}}">
         </label>
+
+        @error('name')
+            <br>
+            <small style="color:red">*{{$message}}</small>
+            <br>
+        @enderror
+
         <br>
         <label >
             Descripcion:
             <br>
-            <textarea name="description" rows="5">{{$curso->description}}</textarea>
+            <textarea name="description" rows="5">{{old('description', $curso->description)}}</textarea>
         </label>
+        
+        @error('description')
+            <br>
+            <small style="color:red">*{{$message}}</small>
+            <br>
+        @enderror
+
         <br>
         <label >
             Categoria:
             <br>
-            <input type="text" name="categoria" value="{{$curso->categoria}}">
+            <input type="text" name="categoria" value="{{old('categoria', $curso->categoria)}}">
         </label>
+
+        @error('categoria')
+            <br>
+            <small style="color:red">*{{$message}}</small>
+            <br>
+        @enderror
+
         <br>
         <input type="submit" value="Enviar formulario">
     </form>
